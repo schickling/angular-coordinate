6123c8192fcfc85872fe307d61a3aaff00700a2a
Johannes Schickling <schickling.j@gmail.com>
2013-09-19 23:53:48 +0200

    Prepared coordinate js

--- src/coordinate.js
+++ src/coordinate.js
@@ -1,12 +1,33 @@
 'use strict';
 
 angular.module('angular-coordinate', [])
-	.directive('coordinate', function() {
+	.directive('coordinate', function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'coordinate.html',
-			link: function(scope, element, attrs) {
-				scope.someContent = 'blub';
+			link: function (scope, element, attrs) {
+
+				function initElement() {
+
+				}
+
+				function initListeners() {
+
+				}
+
+				function provideApi() {
+
+				}
+
+				function draw() {
+
+				}
+
+				initElement();
+				initListeners();
+				provideApi();
+				draw();
+
 			}
 		};
 	});
