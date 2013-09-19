bae0bd24d90c7eab1b6a2fdde05f1b81b4fb8876
Suchanek <d060165@wdfm00277326a.fritz.box>
2013-09-19 23:14:35 +0200

    Updated coordinate.js

--- src/coordinate.js
+++ src/coordinate.js
@@ -5,9 +5,19 @@ angular.module('angular-coordinate', [])
 		return {
 			restrict: 'E',
 			scope: true,
+			transclude: false,
+			replace: false,
 			templateUrl: 'src/coordinate.html'
-			link: function(scope, element, attrs) {
+			compile: function(scope, element, attrs) {
 				var sources = scope.ngModel;
 				scope.someContent = 'Blablabla Mr. Freeman';
+				function someApi(scope, element, attrs) {
+					return {
+						test: function () {
+							console.log('Yeeaahaa');
+						}
+					}
+				}
+				scope.api = new someApi();
 			}
 	})
