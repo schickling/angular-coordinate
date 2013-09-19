4c5801d3145ef04956a81806c73497fe79262404
Johannes Schickling <schickling.j@gmail.com>
2013-09-20 00:50:24 +0200

    Added scaling

--- src/coordinate.js
+++ src/coordinate.js
@@ -7,24 +7,36 @@ angular.module('angular-coordinate', [])
 			templateUrl: 'coordinate.html',
 			link: function (scope, element, attrs) {
 
-				var canvasElement, width, height, ctx, centerPoint, isDragging;
+				var canvasElement, width, height, ctx, centerPoint, isDragging, scaleX, scaleY;
 
-				function initElement() {
-					var coordinateElement = element[0];
+				function initAttibutes() {
 
+					// scale
+					scaleX = attrs.scaleX || 100;
+					scaleY = attrs.scaleY || 100;
+
+					// width
 					width = attrs.width || '700';
-					height = attrs.height || '400';
-					centerPoint = [width/2, height/2];
-					canvasElement = coordinateElement.getElementsByTagName('canvas')[0];
-					ctx = canvasElement.getContext('2d');
-
 					if (width.slice(-1) !== '%') {
 						width += 'px';
 					}
 
+					// height
+					height = attrs.height || '400';
 					if (height.slice(-1) !== '%') {
 						height += 'px';
 					}
+
+					// center point (0, 0)
+					centerPoint = [width / 2, height / 2];
+
+				}
+
+				function initElement() {
+					var coordinateElement = element[0];
+
+					canvasElement = coordinateElement.getElementsByTagName('canvas')[0];
+					ctx = canvasElement.getContext('2d');
 
 					coordinateElement.style.width = canvasElement.style.width = width;
 					coordinateElement.style.height = canvasElement.style.height = height;
@@ -70,9 +82,9 @@ angular.module('angular-coordinate', [])
 					}
 				}
 
-				function api () {
+				function api() {
 					return {
-						drawPoint: function(x,y) {
+						drawPoint: function (x, y) {
 							//ctx.
 						}
 					}
@@ -82,6 +94,7 @@ angular.module('angular-coordinate', [])
 
 				}
 
+				initAttibutes();
 				initElement();
 				initListeners();
 				provideApi();
