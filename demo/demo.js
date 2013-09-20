4c971c877c6d9bb98aa2885836e7b52db8b31d32
vardump <tim.suchanek@gmail.com>
2013-09-20 03:04:29 +0200

    Added test points

--- demo/demo.js
+++ demo/demo.js
@@ -3,8 +3,11 @@
 angular.module('coordinateApp', ['angular-coordinate'])
 	.controller('DemoCtrl', function ($scope) {
 		setTimeout(function () {
-			$scope.coordinate.drawPoint(200,200);
-
+			var points = [[0,0], [1,1], [2,1]]
+			points.forEach(function(point) {
+				$scope.coordinate.addPoint(point[0],point[1]);
+			});
+			$scope.coordinate.addFunction('x^2');
 		},1000);
 
 
